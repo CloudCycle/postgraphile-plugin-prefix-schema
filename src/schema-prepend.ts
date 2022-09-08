0042e5588a8f266748475e6c229305648b37c6a6
Isac Casapu <isac@cloudcycle.com>
2022-09-08 16:34:52 +0100

    Also prepend schema to queries

--- src/schema-prepend.ts
+++ src/schema-prepend.ts
@@ -1,57 +1,4 @@
 import { makeAddInflectorsPlugin } from "graphile-utils";
-import { add, lte } from "sorted-array-functions";
-
-function nameMutationPrefix(name: string): string | undefined {
-    if (! name) {
-        return undefined;
-    }
-    const mutationPrefixes = [
-        'create',
-        'update',
-        'delete',
-        'upsert'
-    ];
-    const nameLc = name[0].toLowerCase() + name.slice(1);
-    return mutationPrefixes.find(
-        s => nameLc.startsWith(s) || name.startsWith(`${s.toUpperCase()}_`)
-    );
-}
-
-class NameMappings {
-    private mappedNames: string[] = [];
-
-    public add(mappedName: string) {
-        if (mappedName.length === 0) {
-            throw new Error("Name mapping resulted in an empty string");
-        }
-        add(this.mappedNames, mappedName);
-    }
-
-    public startsWithMappingOutputName(
-        name: string,
-    ): boolean {
-        for (let lengthToConsider = name.length; lengthToConsider > 0; ) {
-            const closestMappedNameIndex = lte(
-                this.mappedNames,
-                name.slice(undefined, lengthToConsider),
-            );
-            if (closestMappedNameIndex < 0) {
-                break;
-            }
-
-            if (name.startsWith(this.mappedNames[closestMappedNameIndex])) {
-                return true;
-            }
-
-            lengthToConsider = Math.min(
-                this.mappedNames[closestMappedNameIndex].length,
-                lengthToConsider,
-            ) - 1;
-        }
-
-        return false;
-    }
-};
 
 export const SchemaPrepend = makeAddInflectorsPlugin(
     (
@@ -63,7 +10,6 @@ export const SchemaPrepend = makeAddInflectorsPlugin(
             throw new Error("This plugin requires a single schema to be used.");
         }
 
-        const nameMappings = new NameMappings();
         const camelCaseSchema = (inflection.camelCase)(options.pgSchemas[0]);
         const upperCamelCaseSchema = (inflection.upperCamelCase)(options.pgSchemas[0]);
 
@@ -83,31 +29,29 @@ export const SchemaPrepend = makeAddInflectorsPlugin(
                 if (typeof(fResult) !== "string") {
                     return fResult;
                 }
-                else {
-                    const prefixToPreserve = nameMutationPrefix(fResult) ?? '';
-                    const nameAfterPrefix = fResult.slice(prefixToPreserve.length);
-                    if (nameMappings.startsWithMappingOutputName(nameAfterPrefix)) {
-                        return fResult;
-                    }
-                    else {
-                        const transformedName = prependPreservingCase(nameAfterPrefix);
-                        nameMappings.add(transformedName);
-                        return prefixToPreserve + transformedName;
-                    }
-                }
+
+                return prependPreservingCase(fResult);
             };
         }
 
-        const methodsToWrap = Object.entries(inflection).filter(
-            (e) => e[0].endsWith("Type") ||
-                nameMutationPrefix(e[0]) ||
-                ['tableNode'].includes(e[0])
+        const originalInflection = { ...inflection };
+        const functionMethodsWrapped = Object.entries(originalInflection).filter(
+            (e) => e[0].startsWith('function')
         ).map(
             (e) => [e[0], makePrepend(e[1])],
         );
+
+        const { _singularizedTableName, connection } = originalInflection;
+
+        function preserveConnection(...args: any[]) {
+            return connection.apply(originalInflection, args);
+        }
+
         const modifiedInflectors: typeof inflection = {
             ...inflection,
-            ...Object.fromEntries(methodsToWrap),
+            ...Object.fromEntries(functionMethodsWrapped),
+            _singularizedTableName: makePrepend(_singularizedTableName),
+            connection: preserveConnection
         };
         return modifiedInflectors;
     },
